0d14692a8abe954518e288622abe519c70bd0c6b
Anthony Pasquariello <antpas@bu.edu>
2018-01-30 21:24:39 -0500

    Update convertToVideo.py

--- Twitter_Image_Vision/convertToVideo.py
+++ Twitter_Image_Vision/convertToVideo.py
@@ -2,5 +2,4 @@ import cv2
 import os
 
 def save():
-    os.system('ffmpeg -framerate 1/3 -i TwitterImages\image%d.jpg video.avi')
-save()
+    os.system('ffmpeg -y -loglevel panic -framerate 1/3 -i TwitterImages\image%d.jpg video.avi')
