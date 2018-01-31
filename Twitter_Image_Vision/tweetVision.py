1c7d5a82d29d99e59aad8d63603d028fa6290ca2
Anthony Pasquariello <antpas@bu.edu>
2018-01-30 22:21:50 -0500

    Update tweetVision.py

--- Twitter_Image_Vision/tweetVision.py
+++ Twitter_Image_Vision/tweetVision.py
@@ -1,4 +1,10 @@
-import convertToVideo, googleVideo, importTwitter
+import convertToVideo, googleVideo, importTwitter, os
+
+
+def cleanup():
+    for filename in os.listdir('TwitterImages/'):
+        if filename.endswith('.jpg'):
+            os.remove('TwitterImages/' + filename)
 
 
 def segment_label(twitter_username, tweet_count):
@@ -6,7 +12,7 @@ def segment_label(twitter_username, tweet_count):
     importTwitter.download_images(twitter_username, tweet_count)
     convertToVideo.save()
     output = googleVideo.get_segment_labels()
-
+    cleanup()
     return output
 
 
@@ -15,7 +21,7 @@ def shot_label(twitter_username, tweet_count):
     importTwitter.download_images(twitter_username, tweet_count)
     convertToVideo.save()
     output = googleVideo.get_shot_labels()
-
+    cleanup()
     return output
 
 
@@ -24,6 +30,6 @@ def frame_label(twitter_username, tweet_count):
     importTwitter.download_images(twitter_username, tweet_count)
     convertToVideo.save()
     output = googleVideo.get_frame_labels()
-
+    cleanup()
     return output
 
