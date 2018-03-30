e494f7ec6fc8bc67f7b9838081efaeb98f5d687a
Anthony Pasquariello <antpas@bu.edu>
2018-03-29 23:38:31 -0400

    Update script.py

--- Database_Project/script.py
+++ Database_Project/script.py
@@ -1,31 +1,33 @@
 import tweetVision
 import datetime
-import json
 from pymongo import MongoClient
 client = MongoClient()
 
-twitterhandle = 'BU_Tweets'
-num_of_tweets = 200
-out = tweetVision.segment_label(twitterhandle, num_of_tweets)
-keywords = []
-temp = ''
 
-try:
-    for x in range(0, num_of_tweets):
-        keywords.append(out['segmentLabelAnnotations'][x]['entity']['description'])
-except:
-    print("")
+twitterhandles = ['katyperry', 'justinbieber', 'barackobama',
+                  'youtube', 'taylorswift13', 'ladygaga',
+                  'elonmusk', 'richardbranson', 'realdonaldtrump',
+                  'JeffBezos', 'WIRED', 'nytimes','FoxNews',
+                  'cnnbrk', 'jimmyfallon', 'Oprah', 'KingJames',
+                  'instagram', 'NASA', 'TheEconomist', 'NatGeo']
 
-#Mongo Work
-client = MongoClient('localhost', 27017)
-db = client['twitter_analysis']
+for i in range(0, 21):
+    num_of_tweets = 100
+    keywords = []
 
-#Post to db
-posts = db.posts
-post_data = {
-    'twitter_handle': twitterhandle,
-    'keywords': keywords,
-    'number_images': x,
-    'createdon': datetime.datetime.now()
-}
-result = posts.insert_one(post_data)
+    keywords = tweetVision.analyze_labels(twitterhandles[i], num_of_tweets)
+    image_count = tweetVision.cleanup()
+
+    #Mongo Work
+    client = MongoClient('localhost', 27017)
+    db = client['twitter_analysis']
+
+    #Post to db
+    posts = db.posts
+    post_data = {
+        'twitter_handle': twitterhandles[i],
+        'keywords': keywords,
+        'number_images': image_count,
+        'createdon': datetime.datetime.now()
+    }
+    result = posts.insert_one(post_data)
